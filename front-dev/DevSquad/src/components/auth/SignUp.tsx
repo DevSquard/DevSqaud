d5ce3232a375347d3f646c742e484cc822ec3106
jehyun Sung <118592515+protofu@users.noreply.github.com>
2024-09-27 19:49:40 +0900

    feat : sign up

--- front-dev/DevSquad/src/components/auth/SignUp.tsx
+++ front-dev/DevSquad/src/components/auth/SignUp.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ChangeEvent, useState } from "react";
 import style from "./SignUp.module.css";
 
@@ -26,45 +27,56 @@ export default function SignUp() {
     console.log(signUpForm);
   };
 
+  const handleSubmit = await () => {
+    await api
+  };
+
   return (
     <div className={style.container}>
-      <h2>회원가입</h2>
+      <h2 className={style.titleText}>회원가입</h2>
       <div className={style.sign_up_container}>
-      <input
-          type="text"
-          name="name"
-          value={signUpForm.name}
-          onChange={handleChange}
-          placeholder="이름"
-        />
-        <input
-          type="email"
-          name="email"
-          value={signUpForm.email}
-          onChange={handleChange}
-          placeholder="이메일주소"
-        />
-        <input
-          type="password"
-          name="password"
-          value={signUpForm.password}
-          onChange={handleChange}
-          placeholder="비밀번호"
-        />
-        <input
-          type="text"
-          name="nickName"
-          value={signUpForm.nickName}
-          onChange={handleChange}
-          placeholder="사용할 닉네임"
-        />
-        <input
-          type="text"
-          name="birth"
-          value={signUpForm.birth}
-          onChange={handleChange}
-          placeholder="생년월일 8자리, '-' 제외"
-        />
+        <div className={style.sign_up_container_mini}>
+          <input
+            type="email"
+            name="email"
+            value={signUpForm.email}
+            onChange={handleChange}
+            placeholder="이메일주소"
+          />
+          <input
+            type="password"
+            name="password"
+            value={signUpForm.password}
+            onChange={handleChange}
+            placeholder="비밀번호"
+          />
+        </div>
+        <div className={style.sign_up_container_mini}>
+          <input
+              type="text"
+              name="name"
+              value={signUpForm.name}
+              onChange={handleChange}
+              placeholder="이름"
+          />
+          <input
+            type="text"
+            name="nickName"
+            value={signUpForm.nickName}
+            onChange={handleChange}
+            placeholder="사용할 닉네임"
+          />
+          <input
+            type="text"
+            name="birth"
+            value={signUpForm.birth}
+            onChange={handleChange}
+            placeholder="생년월일 8자리, '-' 제외"
+          />
+        </div>
+        <div>
+          <button onClick={handleSubmit}>가입</button>
+        </div>
       </div>
     </div> 
   );
